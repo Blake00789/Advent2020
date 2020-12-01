b27736a24739e1d8669a163a4eac5a845862c302
Blake <jordan00789@gmail.com>
2020-12-01 10:27:00 +0000

    day1

---
+++ AdventOfCode2020/AdventOfCode2020/Day1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AdventOfCode2020
+{
+    class Day1
+    {
+        public static int Part1(List<int> input)
+        {
+            Dictionary<int, int> entries = new Dictionary<int, int>();
+            //converting to a dict gives us O(1) access
+            foreach (var item in input)
+            {
+                entries.Add(item, 0);
+            }
+
+            foreach(var entry in input)
+            {
+                var pair = 2020 - entry;
+                if (entries.ContainsKey(pair))
+                {
+                    return pair * entry;
+                }
+            }
+
+            return 0;
+        }
+
+        public static int Part2(List<int> input)
+        {
+            Dictionary<int, int> entries = new Dictionary<int, int>();
+            //converting to a dict gives us O(1) access
+            foreach (var item in input)
+            {
+                entries.Add(item, 0);
+            }
+
+            foreach (var firstentry in input)
+            {
+                foreach (var secondentry in input)
+                {
+                    if (firstentry == secondentry)
+                    {
+                        continue;
+                    }
+
+                    var thirdentry = 2020 - (firstentry + secondentry);
+                    if (entries.ContainsKey(thirdentry))
+                    {
+                        return firstentry * secondentry * thirdentry;
+                    }
+                }
+            }
+
+            return 0;
+        }
+    }
+}
---
+++ AdventOfCode2020/AdventOfCode2020/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+
+namespace AdventOfCode2020
+{
+    class Program
+    {
+        static List<int> GetInput<Integer>(string file)
+        {
+            string url = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), file);
+            List<int> output = new List<int>();
+
+            using var reader = new StreamReader(url);
+
+            while (!reader.EndOfStream)
+            {
+                var next = reader.ReadLine();
+                var converted = int.Parse(next);
+                output.Add(converted);
+            }
+
+            return output;
+
+        }
+        static void Main(string[] args)
+        {
+            var input = GetInput<int>("day1.txt");
+            Console.WriteLine(Day1.Part2(input));
+            Console.ReadLine();
+        }
+    }
+}
